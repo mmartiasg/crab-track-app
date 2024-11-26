2114f3e599ce022ccf03f79b391ddb406bfe90de
Matias M. Gonzalez <gonzalez.martias@gmail.com>
2024-11-26 23:49:18 +0100

    correct compose entry point command and uncomment test from test_callback.py

--- tests/test_callbacks.py
+++ tests/test_callbacks.py
@@ -34,135 +34,135 @@ class DataloaderSuitCase(unittest.TestCase):
 
         os.makedirs(self.test_videos_output_path, exist_ok=True)
         os.makedirs(self.test_stats_output_path, exist_ok=True)
-    #
-    # def test_interpolate_coordinates_from_sample_2_coordinates_to_frame_0_from_frame_4(self):
-    #     sample_2_coordinates_df = pd.read_csv(os.path.join(os.path.dirname(__file__),
-    #                                                        "test_data/sample_2_coordinates.csv"))
-    #
-    #     interpolated_callback = CallbackInterpolateCoordinatesSingleObjectTracking(coordinates_columns=self.coordinate_columns,
-    #                                                                                method="linear",
-    #                                                                                max_distance=25)
-    #
-    #     interpolated_coordinates_df = interpolated_callback(sample_2_coordinates_df)
-    #
-    #     self.assertTrue(interpolated_coordinates_df.iloc[0][self.coordinate_columns]["x1"],
-    #                     interpolated_coordinates_df.iloc[3][self.coordinate_columns]["x1"])
-    #
-    # def test_denormalized_coordinates_from_sample_1_first_frame_is_1064_85_1343_232(self):
-    #     sample_1_coordinates = pd.read_csv(os.path.join(os.path.dirname(__file__),
-    #                                                     "test_data/sample_1_coordinates.csv"))
-    #
-    #     denormalized_callback = CallbackDenormalizeCoordinates(coordinates_columns=self.coordinate_columns,
-    #                                                            method="xyxy",
-    #                                                            image_size=(1920, 1080))
-    #
-    #     denormalized_coordinates = denormalized_callback(sample_1_coordinates).iloc[0]
-    #
-    #     self.assertEqual(denormalized_coordinates[self.coordinate_columns[0]], 1064)
-    #     self.assertEqual(denormalized_coordinates[self.coordinate_columns[1]], 85)
-    #     self.assertEqual(denormalized_coordinates[self.coordinate_columns[2]], 1343)
-    #     self.assertEqual(denormalized_coordinates[self.coordinate_columns[3]], 232)
-    #
-    # def test_compose_callbacks_using_sample_2_returns_denormalized_coordinates_1289_985_1560_1062_saves_csv_file(
-    #         self):
-    #     sample_2_coordinates_df = pd.read_csv(
-    #                                 os.path.join(
-    #                                     os.path.dirname(__file__),
-    #                                     "test_data/sample_2_coordinates.csv")
-    #                             )
-    #
-    #     r = {"video_name": "sample_2", "coordinates": sample_2_coordinates_df}
-    #
-    #     compose_callback = ComposeCallback([
-    #         CallbackInterpolateCoordinatesSingleObjectTracking(
-    #             coordinates_columns=self.coordinate_columns,
-    #             method="linear",
-    #             max_distance=25),
-    #         CallbackDenormalizeCoordinates(
-    #             coordinates_columns=self.coordinate_columns,
-    #             image_size=(1920, 1080),
-    #             method="xyxy"),
-    #         CallbackSaveToDisk(file_path=os.path.join(
-    #                                 self.test_stats_output_path,
-    #                                 r["video_name"] + "_post_processed.csv"
-    #                             )
-    #         )
-    #     ])
-    #
-    #     sample_2_post_processed = compose_callback(sample_2_coordinates_df).iloc[0]
-    #
-    #     self.assertEqual(sample_2_post_processed[self.coordinate_columns[0]], 1289)
-    #     self.assertEqual(sample_2_post_processed[self.coordinate_columns[1]], 985)
-    #     self.assertEqual(sample_2_post_processed[self.coordinate_columns[2]], 1560)
-    #     self.assertEqual(sample_2_post_processed[self.coordinate_columns[3]], 1062)
-    #     self.assertTrue(os.path.exists(
-    #                                 os.path.join(
-    #                                     self.test_stats_output_path,
-    #                                     r["video_name"] + "_post_processed.csv"
-    #                                 )
-    #                     )
-    #     )
-    #
-    # def test_export_video_with_boundary_and_path_for_sample_video_2_validate_if_file_exists_true(self):
-    #     video_name = "sample_2_test_video"
-    #     sample_2_coordinates_df = pd.read_csv(os.path.join(os.path.dirname(__file__),
-    #                                                        "test_data/sample_2_coordinates.csv"))
-    #
-    #     render_callback = CallbackRenderVideoTracking(output_video_path=os.path.join(self.test_videos_output_path,
-    #                                                                                  video_name + ".mp4"),
-    #                                                   input_video_path=os.path.join(os.path.dirname(__file__),
-    #                                                                         self.config.get_config["input"]["path"],
-    #                                                                         "test_sample_2_720p.mp4"),
-    #                                                   coordinate_columns=self.coordinate_columns,
-    #                                                   bbox_color=(0, 0, 255))
-    #     compose_callback = ComposeCallback([
-    #         CallbackInterpolateCoordinatesSingleObjectTracking(
-    #             coordinates_columns=self.coordinate_columns,
-    #             method="linear",
-    #             max_distance=25),
-    #         CallbackDenormalizeCoordinates(
-    #             coordinates_columns=self.coordinate_columns,
-    #             image_size=self.video_frame_size,
-    #             method="xyxy"),
-    #     ])
-    #     post_processed_coordinates_df = compose_callback(sample_2_coordinates_df)
-    #
-    #     render_callback(post_processed_coordinates_df)
-    #
-    #     self.assertTrue(os.path.exists(os.path.join(self.test_videos_output_path,
-    #                                                 video_name + ".mp4"))
-    #                     )
-    #
-    # def test_export_video_with_boundary_and_path_for_sample_video_1_validate_if_file_exists_true(self):
-    #     video_name = "sample_1_test_video"
-    #     sample_2_coordinates_df = pd.read_csv(os.path.join(os.path.dirname(__file__),
-    #                                                        "test_data/sample_1_coordinates.csv"))
-    #
-    #     render_callback = CallbackRenderVideoTracking(output_video_path=os.path.join(self.test_videos_output_path,
-    #                                                                                  video_name + ".mp4"),
-    #                                                   input_video_path=os.path.join(os.path.dirname(__file__),
-    #                                                                         self.config.get_config["input"]["path"],
-    #                                                                         "test_sample_1_720p.mp4"),
-    #                                                   coordinate_columns=self.coordinate_columns,
-    #                                                   bbox_color=(0, 0, 255))
-    #
-    #     compose_callback = ComposeCallback([
-    #         CallbackInterpolateCoordinatesSingleObjectTracking(
-    #             coordinates_columns=self.coordinate_columns,
-    #             method="linear",
-    #             max_distance=25),
-    #         CallbackDenormalizeCoordinates(
-    #             coordinates_columns=self.coordinate_columns,
-    #             image_size=self.video_frame_size,
-    #             method="xyxy"),
-    #     ])
-    #     post_processed_coordinates_df = compose_callback(sample_2_coordinates_df)
-    #
-    #     render_callback(post_processed_coordinates_df)
-    #
-    #     self.assertTrue(os.path.exists(os.path.join(self.test_videos_output_path,
-    #                                                 video_name + ".mp4"))
-    #     )
+
+    def test_interpolate_coordinates_from_sample_2_coordinates_to_frame_0_from_frame_4(self):
+        sample_2_coordinates_df = pd.read_csv(os.path.join(os.path.dirname(__file__),
+                                                           "test_data/sample_2_coordinates.csv"))
+
+        interpolated_callback = CallbackInterpolateCoordinatesSingleObjectTracking(coordinates_columns=self.coordinate_columns,
+                                                                                   method="linear",
+                                                                                   max_distance=25)
+
+        interpolated_coordinates_df = interpolated_callback(sample_2_coordinates_df)
+
+        self.assertTrue(interpolated_coordinates_df.iloc[0][self.coordinate_columns]["x1"],
+                        interpolated_coordinates_df.iloc[3][self.coordinate_columns]["x1"])
+
+    def test_denormalized_coordinates_from_sample_1_first_frame_is_1064_85_1343_232(self):
+        sample_1_coordinates = pd.read_csv(os.path.join(os.path.dirname(__file__),
+                                                        "test_data/sample_1_coordinates.csv"))
+
+        denormalized_callback = CallbackDenormalizeCoordinates(coordinates_columns=self.coordinate_columns,
+                                                               method="xyxy",
+                                                               image_size=(1920, 1080))
+
+        denormalized_coordinates = denormalized_callback(sample_1_coordinates).iloc[0]
+
+        self.assertEqual(denormalized_coordinates[self.coordinate_columns[0]], 1064)
+        self.assertEqual(denormalized_coordinates[self.coordinate_columns[1]], 85)
+        self.assertEqual(denormalized_coordinates[self.coordinate_columns[2]], 1343)
+        self.assertEqual(denormalized_coordinates[self.coordinate_columns[3]], 232)
+
+    def test_compose_callbacks_using_sample_2_returns_denormalized_coordinates_1289_985_1560_1062_saves_csv_file(
+            self):
+        sample_2_coordinates_df = pd.read_csv(
+                                    os.path.join(
+                                        os.path.dirname(__file__),
+                                        "test_data/sample_2_coordinates.csv")
+                                )
+
+        r = {"video_name": "sample_2", "coordinates": sample_2_coordinates_df}
+
+        compose_callback = ComposeCallback([
+            CallbackInterpolateCoordinatesSingleObjectTracking(
+                coordinates_columns=self.coordinate_columns,
+                method="linear",
+                max_distance=25),
+            CallbackDenormalizeCoordinates(
+                coordinates_columns=self.coordinate_columns,
+                image_size=(1920, 1080),
+                method="xyxy"),
+            CallbackSaveToDisk(file_path=os.path.join(
+                                    self.test_stats_output_path,
+                                    r["video_name"] + "_post_processed.csv"
+                                )
+            )
+        ])
+
+        sample_2_post_processed = compose_callback(sample_2_coordinates_df).iloc[0]
+
+        self.assertEqual(sample_2_post_processed[self.coordinate_columns[0]], 1289)
+        self.assertEqual(sample_2_post_processed[self.coordinate_columns[1]], 985)
+        self.assertEqual(sample_2_post_processed[self.coordinate_columns[2]], 1560)
+        self.assertEqual(sample_2_post_processed[self.coordinate_columns[3]], 1062)
+        self.assertTrue(os.path.exists(
+                                    os.path.join(
+                                        self.test_stats_output_path,
+                                        r["video_name"] + "_post_processed.csv"
+                                    )
+                        )
+        )
+
+    def test_export_video_with_boundary_and_path_for_sample_video_2_validate_if_file_exists_true(self):
+        video_name = "sample_2_test_video"
+        sample_2_coordinates_df = pd.read_csv(os.path.join(os.path.dirname(__file__),
+                                                           "test_data/sample_2_coordinates.csv"))
+
+        render_callback = CallbackRenderVideoTracking(output_video_path=os.path.join(self.test_videos_output_path,
+                                                                                     video_name + ".mp4"),
+                                                      input_video_path=os.path.join(os.path.dirname(__file__),
+                                                                            self.config.get_config["input"]["path"],
+                                                                            "test_sample_2_720p.mp4"),
+                                                      coordinate_columns=self.coordinate_columns,
+                                                      bbox_color=(0, 0, 255))
+        compose_callback = ComposeCallback([
+            CallbackInterpolateCoordinatesSingleObjectTracking(
+                coordinates_columns=self.coordinate_columns,
+                method="linear",
+                max_distance=25),
+            CallbackDenormalizeCoordinates(
+                coordinates_columns=self.coordinate_columns,
+                image_size=self.video_frame_size,
+                method="xyxy"),
+        ])
+        post_processed_coordinates_df = compose_callback(sample_2_coordinates_df)
+
+        render_callback(post_processed_coordinates_df)
+
+        self.assertTrue(os.path.exists(os.path.join(self.test_videos_output_path,
+                                                    video_name + ".mp4"))
+                        )
+
+    def test_export_video_with_boundary_and_path_for_sample_video_1_validate_if_file_exists_true(self):
+        video_name = "sample_1_test_video"
+        sample_2_coordinates_df = pd.read_csv(os.path.join(os.path.dirname(__file__),
+                                                           "test_data/sample_1_coordinates.csv"))
+
+        render_callback = CallbackRenderVideoTracking(output_video_path=os.path.join(self.test_videos_output_path,
+                                                                                     video_name + ".mp4"),
+                                                      input_video_path=os.path.join(os.path.dirname(__file__),
+                                                                            self.config.get_config["input"]["path"],
+                                                                            "test_sample_1_720p.mp4"),
+                                                      coordinate_columns=self.coordinate_columns,
+                                                      bbox_color=(0, 0, 255))
+
+        compose_callback = ComposeCallback([
+            CallbackInterpolateCoordinatesSingleObjectTracking(
+                coordinates_columns=self.coordinate_columns,
+                method="linear",
+                max_distance=25),
+            CallbackDenormalizeCoordinates(
+                coordinates_columns=self.coordinate_columns,
+                image_size=self.video_frame_size,
+                method="xyxy"),
+        ])
+        post_processed_coordinates_df = compose_callback(sample_2_coordinates_df)
+
+        render_callback(post_processed_coordinates_df)
+
+        self.assertTrue(os.path.exists(os.path.join(self.test_videos_output_path,
+                                                    video_name + ".mp4"))
+        )
 
     def test_interpolate_with_max_distance_2_more_a_dataframe_with_4_nan_rows_returns_3_4_5_6_rows_nan(self):
         interpolate_callback = CallbackInterpolateCoordinatesSingleObjectTracking(
